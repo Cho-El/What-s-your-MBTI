1a03c7cf530d9f9662000fde2177811eb96e01d0
송민진 <m_6595@naver.com>
2022-08-04 04:47:51 +0900

    2022-08-04 회원가입 구현 완료

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # flask 기본코드------------------------------
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 app = Flask(__name__)
 
-import json
 import jwt
 import datetime
 import hashlib
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
 
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
@@ -13,16 +14,22 @@ client = MongoClient('localhost', 27017)
 #client = MongoClient('mongodb://test:test@localhost', 27017) # Connection Setting시 유저 네임과 비밀번호를 입력해줘야되요 ex) MongoClient('mongodb://아이디:비번@localhost',27017)
 db = client.mbti
 
-# 성윤님 -----------------------------------------------------
-
-# 병찬님 -----------------------------------------------------
-
-# 민진님 -----------------------------------------------------
-
-# HTML 보여주기
+# HTML 보여주기 - 수민 + 민진 -------------------------------------
 @app.route('/')
 def home():
-    return render_template('sign_up.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        return render_template('index.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("start", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("start", msg="로그인 정보가 존재하지 않습니다."))
+
+@app.route('/start')
+def start():
+    msg = request.args.get("msg")
+    return render_template('sign_up.html', msg=msg)
 
 @app.route('/feature')
 def feature_page():
@@ -36,6 +43,12 @@ def discussion():
 @app.route('/discussion/post')
 def discussion_page():
     return render_template('discussion_post_comments.html')
+
+# 성윤님 -----------------------------------------------------
+
+# 병찬님 -----------------------------------------------------
+
+# 민진님 -----------------------------------------------------
 
 # < 특징 게시판 - 선택한 MBTI의 특징들 가져오기 API >
 @app.route('/api/mbti_features_posts', methods=['POST'])
@@ -65,40 +78,55 @@ def show_comments():
 
 # 수진님 -----------------------------------------------------
 
-
-app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
+# 로그인 API
+@app.route('/api/login', methods=['POST'])
+def log_in():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
 
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'user_id': username_receive, 'user_pw': pw_hash})
 
-@app.route('/login')
-def login():
-    msg = request.args.get("msg")
-    return render_template('login.html', msg=msg)
+    if result is not None:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
-@app.route('/sign_in', methods=['POST'])
-def sign_in():
-    # 로그인
-    return jsonify({'result': 'success'})
-
-# 회원가입
-@app.route('/sign_up/save', methods=['POST'])
+# 회원가입 API
+@app.route('/api/signup/save', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
+    nickname_receive = request.form['nickname_give']
     mbti_receive = request.form['mbti_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "username": username_receive,                               # 아이디
-        "password": password_hash,                                  # 비밀번호
-        "mbti": mbti_receive                                        # mbti
+        "user_id": username_receive,                               # 아이디
+        "user_pw": password_hash,                                  # 비밀번호
+        "user_nickname": nickname_receive,                          # 닉네임
+        "user_mbti": mbti_receive                                  # mbti
     }
-    db.users.insert_one(doc)
+    db.User.insert_one(doc)
     return jsonify({'result': 'success'})
 
+# 중복확인 API
+@app.route('/api/signup/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    # print(value_receive, type_receive, exists)
+    return jsonify({'result': 'success', 'exists': exists})
 
 # 수민님 -----------------------------------------------------
 
